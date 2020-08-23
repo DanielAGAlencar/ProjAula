2fcbd9882e85717be6dc19834e251283a1bdea32
Alencar <alencar@gvmsolucoes.com.br>
2020-08-23 20:25:19 -0300

    commit

---
+++ ProjAula/CadastroAula.aspx.cs
@@ -0,0 +1,73 @@
+﻿using Dados;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjAula
+{
+    public partial class CadastroAula : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadDataPage();
+            }
+        }
+
+        protected void BtnSalvar_Click(object sender, EventArgs e)
+        {
+
+            string idItem = Request.QueryString["idItem"];
+
+            Aula aula = new Aula()
+            {
+                NomeDisciplina = TxtNomeDisciplina.Text,
+                Qtd_Alunos = int.Parse(TxtQtd_Alunos.Text),
+                NomeProfessor = TxtNomeProfessor.Text,
+                NomeFaculdade = TxtNomeFaculdade.Text
+            };
+
+            if (String.IsNullOrEmpty(idItem))
+            {
+                new AulaDB().Save(aula);
+                lblMSG.Text = "Registro Inserido!";
+            }
+            else
+            {
+                aula.Id = int.Parse(idItem);
+                new AulaDB().Update(aula);
+                lblMSG.Text = "Registro Atualizado!";
+            }
+        }
+
+        protected void BtnVoltar_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Default.aspx");
+        }
+
+        private void LoadDataPage()
+        {
+            string idItem = Request.QueryString["idItem"];
+
+            if (!String.IsNullOrEmpty(idItem))
+            {
+                Aula aula = new AulaDB().FindById(int.Parse(idItem));
+
+                lblId.Visible = true;
+                TxtId.Visible = true;
+
+                TxtQtd_Alunos.Text = aula.Qtd_Alunos.ToString();
+                TxtNomeDisciplina.Text = aula.NomeDisciplina;
+                TxtId.Text = aula.Id.ToString();
+                TxtNomeProfessor.Text = aula.NomeProfessor;
+                TxtNomeFaculdade.Text = aula.NomeFaculdade;
+            }
+        }
+    }
+}
---
+++ ProjAula/Default.aspx.cs
@@ -0,0 +1,65 @@
+﻿using Dados;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjAula
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LoadTable();
+        }
+
+        private void LoadTable()
+        {
+            GDVAula.DataSource = new AulaDB().FindAll();
+            GDVAula.DataBind();
+        }
+
+        protected void BtnInserir_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("CadastroAula.aspx");
+        }
+
+        protected void GDVAula_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            int line = int.Parse(e.CommandArgument.ToString());
+            int id = int.Parse(GDVAula.Rows[line].Cells[0].Text);
+
+            Aula bilheteria = new AulaDB().FindById(id);
+
+            if (e.CommandName == "A")
+            {
+                Response.Redirect("CadastroAula.aspx?idItem=" + id);
+            }
+            else if (e.CommandName == "E")
+            {
+                lblExcluir.Text = id.ToString();
+                lblMsg.Text = "Tem certeza que deseja excluir este registro?";
+                DisplayModal(this);
+            }
+        }
+
+        private void DisplayModal(Page page)
+        {
+            ClientScript.RegisterStartupScript(typeof(Page),
+                                               Guid.NewGuid().ToString(),
+                                               "MostrarModal();",
+                                               true);
+        }
+
+        protected void btnConfirm_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(lblExcluir.Text);
+            new AulaDB().Delete(id);
+            LoadTable();
+        }
+    }
+}
